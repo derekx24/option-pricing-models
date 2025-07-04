96eb8c65eccdc54e32fe974e13bf2fa329ebf16a
Derek Xu <derekxu08@gmail.com>
2025-07-04 14:24:27 -0700

    implement greeks

--- models/black_scholes.py
+++ models/black_scholes.py
@@ -24,3 +24,34 @@ def black_scholes_price(S, K, T, r, sigma, option_type="call"):
         return K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
     else:
         raise ValueError("option_type must be 'call' or 'put'")
+
+def delta(S, K, T, r, sigma, option_type="call"):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2)*T) / (sigma * np.sqrt(T))
+    if option_type == "call":
+        return norm.cdf(d1)
+    elif option_type == "put":
+        return norm.cdf(d1) - 1
+
+def gamma(S, K, T, r, sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2)*T) / (sigma * np.sqrt(T))
+    return norm.pdf(d1) / (S * sigma * np.sqrt(T))
+
+def vega(S, K, T, r, sigma):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2)*T) / (sigma * np.sqrt(T))
+    return S * norm.pdf(d1) * np.sqrt(T)
+
+def theta(S, K, T, r, sigma, option_type="call"):
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2)*T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    term1 = -(S * norm.pdf(d1) * sigma) / (2 * np.sqrt(T))
+    if option_type == "call":
+        return term1 - r * K * np.exp(-r * T) * norm.cdf(d2)
+    elif option_type == "put":
+        return term1 + r * K * np.exp(-r * T) * norm.cdf(-d2)
+
+def rho(S, K, T, r, sigma, option_type="call"):
+    d2 = (np.log(S / K) + (r - 0.5 * sigma**2)*T) / (sigma * np.sqrt(T))
+    if option_type == "call":
+        return K * T * np.exp(-r * T) * norm.cdf(d2)
+    elif option_type == "put":
+        return -K * T * np.exp(-r * T) * norm.cdf(-d2)
